b4f31eb2fc817ab6159e28be1bd2222e152b83e2
bishnu <das.2312bishnu@gmail.com>
2023-04-25 21:04:08 +0530

    9. Declarative/Functional style approach

--- src/test/java/lectures/Lecture1.java
+++ src/test/java/lectures/Lecture1.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import beans.Person;
 
 import com.google.common.collect.ImmutableList;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,27 +17,30 @@ import org.junit.Test;
 
 public class Lecture1 {
 
-  @Test
-  public void imperativeApproach() throws IOException {
-    List<Person> people = MockData.getPeople();
-    // 1. Find people aged less or equal 18
+    @Test
+    public void imperativeApproach() throws IOException {
+        List<Person> people = MockData.getPeople();
+        // 1. Find people aged less or equal 18
 //    for(Person person: people){
 //      if(person.getAge() <= 18){
 //        System.out.println(person.getFirstName());
 //      }
 //    }
-    // 2. Then change implementation to find first 10 people
-    for(int i=0; i<10; i++){
-      Person person = people.get(i);
-      if(person.getAge() <= 18){
-        System.out.println(person.getFirstName());
-      }
+        // 2. Then change implementation to find first 10 people
+        for (int i = 0; i < 10; i++) {
+            Person person = people.get(i);
+            if (person.getAge() <= 18) {
+                System.out.println(person.getFirstName());
+            }
+        }
     }
-  }
 
-  @Test
-  public void declarativeApproachUsingStreams() throws Exception {
-    ImmutableList<Person> people = MockData.getPeople();
-
-  }
+    @Test
+    public void declarativeApproachUsingStreams() throws Exception {
+        MockData.getPeople().stream()
+                .filter(person -> person.getAge() <= 18)
+                .limit(10)
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+    }
 }
