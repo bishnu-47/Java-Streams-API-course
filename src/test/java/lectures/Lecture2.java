6f8d3e18af5cc72226c1851163e47275e8c18d50
bishnu <das.2312bishnu@gmail.com>
2023-04-26 16:48:17 +0530

    11. Iterating with IntStream

--- src/test/java/lectures/Lecture2.java
+++ src/test/java/lectures/Lecture2.java
@@ -10,20 +10,23 @@ public class Lecture2 {
 
   @Test
   public void range() throws Exception {
+    System.out.println("Range Exclusive");
+    IntStream.range(0, 10).forEach(System.out::println);
 
+    System.out.println("Range Inclusive");
+    IntStream.rangeClosed(0, 10).forEach(System.out::println);
   }
 
   @Test
   public void rangeIteratingLists() throws Exception {
     List<Person> people = MockData.getPeople();
-
   }
 
   @Test
   public void intStreamIterate() throws Exception {
-      IntStream.iterate(0, operand -> operand + 1)
-          .filter(number -> number % 2 == 0)
-          .limit(20)
-          .forEach(System.out::println);
+    IntStream.iterate(0, operand -> operand + 1)
+        .filter(number -> number % 2 == 0)
+        .limit(20)
+        .forEach(System.out::println);
   }
 }
