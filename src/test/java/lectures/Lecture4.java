94a2b96c6b500882ea89da3cb11c4579a772147c
bishnu <das.2312bishnu@gmail.com>
2023-04-27 18:14:44 +0530

    18. Removing duplicates in a list

--- src/test/java/lectures/Lecture4.java
+++ src/test/java/lectures/Lecture4.java
@@ -1,6 +1,5 @@
 package lectures;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.google.common.collect.ImmutableList;
@@ -13,13 +12,19 @@ public class Lecture4 {
 
   @Test
   public void distinct() throws Exception {
-    final List<Integer> numbers = ImmutableList.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
+    final List<Integer> numbers =
+        ImmutableList.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
 
+    List<Integer> distinctNumbers = numbers.stream().distinct().collect(Collectors.toList());
+
+    System.out.println(distinctNumbers);
+
+    assertThat(distinctNumbers).hasSize(9);
   }
 
   @Test
   public void distinctWithSet() throws Exception {
-    final List<Integer> numbers = ImmutableList.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
-
+    final List<Integer> numbers =
+        ImmutableList.of(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 9, 9, 9);
   }
 }
