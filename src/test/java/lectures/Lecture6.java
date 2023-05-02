00526daaed28f88a9f76c7181f0e857b38f1b744
bishnu <das.2312bishnu@gmail.com>
2023-05-02 19:13:59 +0530

    section 8: Find Any vs Find First

--- src/test/java/lectures/Lecture6.java
+++ src/test/java/lectures/Lecture6.java
@@ -1,6 +1,5 @@
 package lectures;
 
-
 import com.google.common.collect.Lists;
 import java.util.Arrays;
 import java.util.function.Predicate;
@@ -13,13 +12,17 @@ public class Lecture6 {
   @Test
   public void findAny() throws Exception {
     Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    Integer integer = Arrays.stream(numbers).filter(numbersLessThan10).findAny().get();
 
+    System.out.println(integer);
   }
 
   @Test
   public void findFirst() throws Exception {
     Integer[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+    Integer integer = Arrays.stream(numbers).filter(numbersLessThan10).findFirst().get();
+
+    System.out.println(integer);
   }
 }
-
